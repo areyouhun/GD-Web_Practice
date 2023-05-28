a834c2b97be5db68c2c08874d6425566aa3c2ab8
areyouhun <areyouhun@gmail.com>
2023-05-29 03:38:50 +0900

    feat(dto): DB의 회원정보를 담는 dto 생성

--- src/main/java/com/web/member/controller/LogInServlet.java
+++ src/main/java/com/web/member/controller/LogInServlet.java
@@ -1,12 +1,14 @@
 package com.web.member.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+
+import com.web.member.model.dto.Member;
 
 @WebServlet("/login.do")
 public class LogInServlet extends HttpServlet {
